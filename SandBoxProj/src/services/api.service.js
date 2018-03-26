b9400b5ee6ceca5ed76e71ed72d655e6f9f4781f
Steven Timothy <steven.timothy.cp@gmail.com>
2018-03-26 14:13:50 -0600

    added api service init data and searches

---
+++ SandBoxProj/src/services/api.service.js
@@ -0,0 +1,57 @@
+let ApiService = class ApiService 
+{
+    constructor() 
+    {
+		this.apiProtocol = 'https:';
+		this.apiHost = 'swapi.co/api/';
+	}
+    
+    get apiLocation() 
+    {
+      	return `${this.apiProtocol}//${this.apiHost}`;
+    }
+
+    getData()
+    {
+        return `${this.apiLocation}films/`
+    }
+
+    //searches for titles of films.
+    searchFilms(term)
+    {
+        return `${this.apiLocation}films/?search=${term}`
+    }
+
+    //searches for names of people
+    searchPeople(term)
+    {
+        return `${this.apiLocation}people/?search=${term}`
+    }
+
+    //searches for names of planets
+    searchPlanets(term)
+    {
+        return `${this.apiLocation}planets/?search=${term}`
+    }
+
+    //searches for names of planets
+    searchSpecies(term)
+    {
+        return `${this.apiLocation}species/?search=${term}`
+    }
+
+    //searches for names or models of starships
+    searchStarships(term)
+    {
+        return `${this.apiLocation}starships/?search=${term}`
+    }
+
+    //searches for names and models of vehicles
+    searchVehicles(term)
+    {
+        return `${this.apiLocation}vehicles/?search=${term}`
+    }
+};
+
+const apiService = new ApiService();
+export default apiService;
